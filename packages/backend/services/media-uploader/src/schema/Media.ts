135e7f3095528d5a4c8342d663ff0d484c6eeafa
nadirlaskar <nadirlaskar@gmail.com>
2023-07-23 17:00:11 +0530

    add validation

--- packages/backend/services/media-uploader/src/schema/Media.ts
+++ packages/backend/services/media-uploader/src/schema/Media.ts
@@ -13,10 +13,10 @@ export const mediaSchema: JSONSchemaType<Media> = {
   $id: MEDIA_SCHEMA_NAME,
   type: "object",
   properties: {
-    title: { type: "string" },
-    description: { type: "string" },
-    tags: { type: "array", items: { type: "string" } },
-    type: { type: "string" }
+    title: { type: "string", minLength: 4, maxLength: 100 },
+    description: { type: "string", minLength: 30, maxLength: 1000 },
+    tags: { type: "array", items: { type: "string", minLength:4, maxLength: 20 } },
+    type: { type: "string", enum: ["image", "video", "audio", "list"] }
   },
   required: ["title", "description", "tags", "type"],
   additionalProperties: false
